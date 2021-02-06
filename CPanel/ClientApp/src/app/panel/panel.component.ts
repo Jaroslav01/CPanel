75cfa4bcb31018d72ddc6459056eca9479a980c8
Yaroslav Yushchenko <azazazazazazazzaza1@gmail.com>
2021-02-06 13:17:14 +0200

    060220211317

--- CPanel/ClientApp/src/app/panel/panel.component.ts
+++ CPanel/ClientApp/src/app/panel/panel.component.ts
@@ -1,40 +1,9 @@
-
-import { HttpClient } from '@angular/common/http';
-import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'panel-component',
-  templateUrl: './panel.component.html'
+  selector: 'app-panel',
+  templateUrl: './panel.component.html',
 })
 export class PanelComponent {
-  public response: Parameter[];
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Parameter[]>(baseUrl + 'mqtt/GetParameters').subscribe(result => {
-      this.response = result;
-    }, error => console.error(error));
-  }
-
-  public currentCount = 0;
-  public i = 1;
-  public topic: string;
-  public OnOff2(topic2: string) {
-    this.currentCount++;
-    this.i = 1;
-    if (this.currentCount % 2 == 0) { this.i = 0; }
-    else { this.i = 1; }
-    var request = new Request("https://localhost:44333/mqtt/set?topic=" + topic2 + "&value=" + this.i);
-    fetch(request).then(function (response) {
-      return response.text();
-    }).then(function (text) {
-      console.log(text.substring(0, 30));
-    });
-  }
 }
 
-interface Parameter {
-  id: number;
-  deviseId: number;
-  name: string;
-  topic: string;
-  data: string;
-}
